4cb6051c4a0861d7d8507de0f9da0bc722442c80
phuongnam7899 <phuongnam7899@gmail.com>
2025-10-29 18:42:32 +0700

    update y song

--- src/environments/environment.yen.ts
+++ src/environments/environment.yen.ts
@@ -6,7 +6,7 @@ export const environment = {
   appTitle: "Yen's Music",
   audioBasePath: 'assets/music/yen',
   songs: [
-    { id: 1, title: 'Anh Den Pho', filename: 'anh_den_pho.mp3' },
+    { id: 1, title: 'arcane', filename: 'arcane.mp3' },
     // Add more Yen's songs here
   ],
 };
