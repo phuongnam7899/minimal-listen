c48dfc6578cd11f4b938e939960bc0afcc3f459e
phuongnam7899 <phuongnam7899@gmail.com>
2025-12-23 00:03:58 +0700

    update vanh music

--- src/environments/environment.vanh.ts
+++ src/environments/environment.vanh.ts
@@ -6,7 +6,10 @@ export const environment = {
   appTitle: "Vanh's Music",
   audioBasePath: 'assets/music/vanh',
   songs: [
-    { id: 1, title: 'Rain', filename: 'rain.mp3' },
+    { id: 1, title: 'Rain', filename: 'rain2.opus' },
+    { id: 2, title: 'Rain 2', filename: 'rain2.opus' },
+    { id: 3, title: 'Rain 3', filename: 'rain2.opus' },
+    { id: 4, title: 'Rain 4', filename: 'rain2.opus' },
     // Add Vanh's songs here
   ],
 };
